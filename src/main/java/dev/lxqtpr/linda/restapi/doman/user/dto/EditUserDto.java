4b24e3cea8645df97161552f506d79cfb83a94ec
Bruno <lxqtpr@gmaill.com>
2024-06-06 16:53:43 +0300

    add prod yml

--- src/main/java/dev/lxqtpr/linda/restapi/doman/user/dto/EditUserDto.java
+++ src/main/java/dev/lxqtpr/linda/restapi/doman/user/dto/EditUserDto.java
@@ -3,9 +3,11 @@ package dev.lxqtpr.linda.restapi.doman.user.dto;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class EditUserDto {
     @NotNull
     private Long telegramChatId;
